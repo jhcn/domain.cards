27aeac1b1f20e39dbac5bd0666d2be4dba954c6d
xiongbao <yns@qq.com>
2024-01-09 20:18:36 +0800

    feat: templates dev

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       boxShadow: {
         bento: '0 2px 4px 0 rgb(0 0 0/.04)',
-        avatar: '0 1px 2px 0 rgb(0 0 0/.04)'
+        avatar: '0 1px 2px 0 rgb(0 0 0/.1)'
       },
       borderRadius: {
         bento: '1.25rem'
