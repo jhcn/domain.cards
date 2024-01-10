3ccdf95eb6283ef220a4d0939f24b7a12831441d
xiongbao <yns@qq.com>
2024-01-10 08:44:04 +0800

    feat: teamplate dev

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./templates/*.{html,js}'],
   theme: {
     extend: {
+      fontFamily: {
+        ubuntu: ['ubuntu'],
+      },
       boxShadow: {
         bento: '0 2px 4px 0 rgb(0 0 0/.04)',
         avatar: '0 1px 2px 0 rgb(0 0 0/.1)'
